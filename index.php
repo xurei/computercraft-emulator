3fbdec39e709533b63bd5d1f8e9818d4af542913
Olivier Bourdoux <olivier.bourdoux@gmail.com>
2014-09-11 17:47:54 +0200

    Google Analytics

--- index.php
+++ index.php
@@ -88,5 +88,19 @@
 
 	<script src="js/ui.js"></script>
 	
+	<?php if (true || !file_exists("/home/olivier")): ?>
+	<?php $ga_code=""; if (file_exists(dirname(__FILE__)."/ga_code.txt")) $ga_code=file_get_contents(dirname(__FILE__)."/ga_code.txt"); ?>
+		<script>
+			var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', '<?= $ga_code ?>']);
+		  _gaq.push(['_trackPageview']);
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		</script>
+	<?php endif ?>
+	
 </body>
 </html>
