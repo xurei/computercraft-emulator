f04e3af3d7ae9575336660d8395eaee7b68c9606
Olivier Bourdoux <olivier.bourdoux@gmail.com>
2014-10-12 16:48:06 +0200

    Force reload the worker on error

--- js/lua_worker.js
+++ js/lua_worker.js
@@ -4,11 +4,11 @@ importScripts("lua5.1.5.js"/*, "lua.vm.extend.js"*/);
 var global = this;
 var call_return = null;
 global.CCAPI = {};
+global.ready = false;
 global.running = false;
 
-var C = Lua5_1.C;
-var L = C.lua_open();
-C.luaL_openlibs(L);
+var C = null;
+var L = null;
 //------------------------------------------------------------------------------
 
 function isInt(n) {
@@ -58,12 +58,12 @@ function load_lua(src) {
 	var xhr = new XMLHttpRequest();
 	xhr.open("GET", src, false);
 	xhr.send();
-	//L.execute(xhr.responseText);
 	if (C.luaL_dostring(L, xhr.responseText) != 0)
 	{
 	  var err = C.lua_tostring(L, -1);
 	  C.lua_close(L);
 	  L = 0;
+		global.ready = false;
 	  throw new Error("Lua error in " + src + " : " + err);
 	}
 	/*else
@@ -71,66 +71,84 @@ function load_lua(src) {
 		console.debug(src+ "loaded");
 	}*/
 }
-load_lua('../lua/math.lua')
-load_lua('../lua/bit.lua')
-load_lua('../lua/sleep.lua')
-load_lua('../lua/colors.lua')
-load_lua('../lua/util.lua	')
-load_lua('../lua/tabletojson.lua')
-load_lua('../lua/peripheral.lua')
+//------------------------------------------------------------------------------
+
+function init()
+{
+	console.log('INIT');
+	C = Lua5_1.C;
+	L = C.lua_open();
+	C.luaL_openlibs(L);
+	
+	load_lua('../lua/math.lua');
+	load_lua('../lua/bit.lua');
+	load_lua('../lua/sleep.lua');
+	load_lua('../lua/colors.lua');
+	load_lua('../lua/util.lua	');
+	load_lua('../lua/tabletojson.lua');
+	load_lua('../lua/peripheral.lua');
+	//----------------------------------------------------------------------------
+
+	C.lua_pushcfunction(
+		L,
+		Lua5_1.Runtime.addFunction(function(L)
+		{
+			var args = [];
+			var str;
+			var i = 1;
+			while ((str = C.lua_tostring(L, i)) != "") {
+				args.push(str);
+				i++;
+			}
+			var out = "";
+			var sep = "";
+			for (i in args){
+				out += sep + args[i];
+				sep = "\t"; 
+			}
+
+			global.postMessage( {type:"PRINT", data:out} );
+			return 0;
+		})
+	);
+	C.lua_setglobal(L, "_print");
+	//----------------------------------------------------------------------------
+
+	//Message sending function
+	C.lua_pushcfunction(
+		L,
+		Lua5_1.Runtime.addFunction(function(L)
+		{
+			var json = C.luaL_checklstring(L, 1);
+			var obj = JSON.parse(json);
+			//console.log("SEND MESSAGE");
+			
+			global.postMessage(obj);
+			
+			return 0;
+		})
+	);
+	C.lua_setglobal(L, "SEND_MESSAGE");
+	//----------------------------------------------------------------------------
+	
+	global.ready = true;
+}
 //------------------------------------------------------------------------------
 
 function execute(code) {
+	if (!global.ready)
+		init();
 	if (C.luaL_dostring(L, code) != 0)
 	{
 	  var err = C.lua_tostring(L, -1);
 	  C.lua_close(L);
 	  L = 0;
+		global.ready = false;
+		global.running = false;
+		global.postMessage({type:"END"});
 	  throw new Error("Lua error : " + err);
 	}
 }
-//------------------------------------------------------------------------------
-
-C.lua_pushcfunction(
-	L,
-	Lua5_1.Runtime.addFunction(function(L)
-	{
-		var args = [];
-		var str;
-		var i = 1;
-		while ((str = C.lua_tostring(L, i)) != "") {
-			args.push(str);
-			i++;
-		}
-		var out = "";
-		var sep = "";
-		for (i in args){
-			out += sep + args[i];
-			sep = "\t"; 
-		}
-
-		global.postMessage( {type:"PRINT", data:out} );
-		return 0;
-	})
-);
-C.lua_setglobal(L, "_print");
-//------------------------------------------------------------------------------
-
-//Message sending function
-C.lua_pushcfunction(
-	L,
-	Lua5_1.Runtime.addFunction(function(L)
-	{
-		var json = C.luaL_checklstring(L, 1);
-		var obj = JSON.parse(json);
-		//console.log("SEND MESSAGE");
-		
-		global.postMessage(obj);
-		
-		return 0;
-	})
-);
-C.lua_setglobal(L, "SEND_MESSAGE");
 //------------------------------------------------------------------------------
 
 global.CCAPI.resume = function(values) { 
@@ -156,9 +174,10 @@ addEventListener("message", function(e) {
 	{
 		case 'START': {
 			global.running = true;
+			console.log('START');
 			
 			//Pushing the peripherals' signature into the Lua VM
-			execute("peripheral.periphs = "+jsontotable(e.data.peripherals));
+			execute("print('ok')\nperipheral.periphs = "+jsontotable(e.data.peripherals));
 			
 			//Defining the code as a coroutine
 			var code = "main_routine = coroutine.create(function ()\n" + e.data.code + "\n SEND_MESSAGE(\"{\\\"type\\\":\\\"END\\\"}\")\nend)";
